cbe9fcf26d81e107d5eeef9bfb4e9981d0029aeb
James Palacios <james.hollywood69@hotmail.com>
2021-05-11 16:55:42 -0500

    modificando configuracion asgi

--- scripts/send_tracklog_data.py
+++ scripts/send_tracklog_data.py
@@ -56,8 +56,6 @@ while(redisClient.llen(queue_name)!=0):
         </TypeInfo>
     """
     items.append(xml_item)
-    print(xml_item)
-
 
 xml_header = """<?xml version="1.0" encoding="utf-8"?>
 <soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">
