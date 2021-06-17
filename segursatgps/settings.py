5edc4e7b154fa3e027945b3f16f1746f0d5851ac
James Palacios <james.hollywood69@hotmail.com>
2021-06-17 15:32:20 -0500

    añadir autenticacion a rest framework

--- segursatgps/settings.py
+++ segursatgps/settings.py
@@ -52,6 +52,16 @@ INSTALLED_APPS = [
     'maintenances',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
