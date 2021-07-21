4c6985483d77b669db31ddf571740859b5f5795f
James Palacios <james.hollywood69@hotmail.com>
2021-07-21 13:10:46 -0500

    añadiendo barra de busqueda al admin de django

--- geofences/admin.py
+++ geofences/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Geofence
 
+@admin.register(Geofence)
+class GeofenceAdmin(admin.ModelAdmin):
+    search_fields = ['name','description',]
+
 # Register your models here.
-admin.site.register([Geofence])
+#admin.site.register([Geofence])
