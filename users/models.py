ee23491ed4a857d5dae0684d54416090ec7774c3
James Palacios <james.hollywood69@hotmail.com>
2021-08-10 11:35:36 -0500

    blank=true profile units

--- users/models.py
+++ users/models.py
@@ -20,7 +20,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     account = models.ForeignKey(Account,on_delete=models.CASCADE)
     is_admin = models.BooleanField(default=False)
-    units = models.ManyToManyField(Device)
+    units = models.ManyToManyField(Device,blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     def __str__(self):
