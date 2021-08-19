333ffb9a72a9874dadd039c24d7258a24888f471
James Palacios <james.hollywood69@hotmail.com>
2021-08-19 12:10:20 -0500

    Agregando FleetTriggerExtension1008 3

--- triggers/admin.py
+++ triggers/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import FleetTrigger,FleetTriggerExtension1003,FleetTriggerExtension1006,FleetTriggerExtension1007
+from .models import FleetTrigger,FleetTriggerExtension1003,FleetTriggerExtension1006,FleetTriggerExtension1007,FleetTriggerExtension1008
 
 # Register your models here.
-admin.site.register([FleetTrigger,FleetTriggerExtension1003,FleetTriggerExtension1006,FleetTriggerExtension1007])
+admin.site.register([
+  FleetTrigger,
+  FleetTriggerExtension1003,
+  FleetTriggerExtension1006,
+  FleetTriggerExtension1007,
+  FleetTriggerExtension1008,
+])
