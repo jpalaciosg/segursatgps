a8a39acdb02cebe4ebe77312f06c67309c16ff06
James Palacios <james.hollywood69@hotmail.com>
2021-08-23 17:10:04 -0500

    reporte detallado con kilometaje

--- reports/forms.py
+++ reports/forms.py
@@ -31,3 +31,9 @@ class GroupSpeedReportForm(forms.Form):
     final_datetime = forms.CharField(max_length=50,required=True,label='Fecha final')
     group_name = forms.CharField(max_length=50,required=True,label='Grupo')
     speed_limit = forms.IntegerField(required=True)
+
+class DetailedMileageReportForm(forms.Form):
+    initial_datetime = forms.CharField(max_length=50,required=True,label='Fecha inicial')
+    final_datetime = forms.CharField(max_length=50,required=True,label='Fecha final')
+    unit_name = forms.CharField(max_length=50,required=True,label='Unidad')
+    mileage = forms.IntegerField(required=True)
