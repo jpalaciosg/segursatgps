12baffa10ed9d908e66b94a0e6bfc7c1d56a2ca5
James Palacios <james.hollywood69@hotmail.com>
2021-10-11 13:43:24 -0500

    reparando el group speed report

--- reports/views.py
+++ reports/views.py
@@ -1632,14 +1632,6 @@ def group_speed_report_view(request):
                         'address':location_qs.address,
                         'attributes':json.loads(location_qs.attributes),
                     })
-                if len(locations) == 0:
-                    return render(request,'reports/group-speed-report.html',{
-                        'initial_datetime':data['initial_datetime'],
-                        'final_datetime':data['final_datetime'],
-                        'groups':groups,
-                        'form':form,
-                        'error':'No existe un recorrido para analizar.'
-                    })
                 device_reader = DeviceReader(unit.uniqueid)
                 speed_limit = int(data['speed_limit'])
                 speed_report = device_reader.generate_speed_report(locations,speed_limit)
