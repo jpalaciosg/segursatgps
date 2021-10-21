ea17877544c2dcd05c8b3a602c92930cf72b5e84
James Palacios <james.hollywood69@hotmail.com>
2021-10-21 16:45:52 -0500

    corrigiendo reporte de telemetria 4

--- reports/views.py
+++ reports/views.py
@@ -2367,7 +2367,7 @@ def telemetry_report_view(request):
                 except:
                     locations[i].total_fuel_used = 'N/D'
                 try:
-                    locations[i].odometer = attributes['io192']
+                    locations[i].odometer = round(float(attributes['io192'])/1000,2)
                 except:
                     locations[i].odometer = 'N/D'
             return render(request,'reports/telemetry-report.html',{
