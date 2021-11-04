554fd1be9068e15e264bff8f90c4b8732d65333e
James Palacios <james.hollywood69@hotmail.com>
2021-11-04 16:04:28 -0500

    creando get_alert_view

--- alerts/views.py
+++ alerts/views.py
@@ -25,120 +25,106 @@ def alerts_view(request):
     return render(request,'alerts/alerts.html')
 
 def alert_history_view(request):
-    if request.method == 'POST':
-        data = request.POST
-        units = Device.objects.filter(account=request.user.profile.account)
-        initial_timestamp = None
-        final_timestamp = None
-        form = AlertHistoryForm(data)
-        if form.is_valid():
-            unit = None
-            if data['unit_name'].upper() != 'ALL':
-                try:
-                    unit = Device.objects.get(name=data['unit_name'])
-                except Exception as e:
-                    print(e)
-                    form.add_error('unit_name', e)
-            #
-            try:
-                initial_datetime_str = f"{data['initial_datetime']}:00"
-                initial_datetime_obj = datetime.strptime(initial_datetime_str, '%Y-%m-%d %H:%M:%S')
-                # convertir a zona horaria
-                initial_datetime_obj = gmt_conversor.convert_localtimetoutc(initial_datetime_obj)
-                # --
-                initial_timestamp = datetime.timestamp(initial_datetime_obj)
-            except Exception as e:
-                print(e)
-                form.add_error('initial_date', e)
-            #
-            try:
-                final_datetime_str = f"{data['final_datetime']}:00"
-                final_datetime_obj = datetime.strptime(final_datetime_str, '%Y-%m-%d %H:%M:%S')
-                # convertir a zona horaria
-                final_datetime_obj = gmt_conversor.convert_localtimetoutc(final_datetime_obj)
-                # --
-                final_timestamp = datetime.timestamp(final_datetime_obj)
-            except Exception as e:
-                form.add_error('final_date', e)
-
-            if len(form.errors) != 0:
-                return render(request,'alerts/alert-history.html',{
-                    'units':units,
-                    'form':form,
-                })
-
-            if data['unit_name'].upper() == 'ALL':
-                alerts = Alert.objects.using('history_db_replica').filter(
-                    accountid=request.user.profile.account.id,
-                    timestamp__gte=initial_timestamp,
-                    timestamp__lte=final_timestamp,
-                    alert_type=data['alert_type']
-                ).order_by('id')
-                for alert in alerts:
-                    unit = units.get(id=alert.unitid)
-                    alert.unit_name = unit.name
-                    alert.unit_description = unit.description
-                    dt = datetime.utcfromtimestamp(alert.timestamp)
-                    dt = gmt_conversor.convert_utctolocaltime(dt) # convertir a zona horaria
-                    alert.datetime = dt.strftime("%d/%m/%Y %H:%M:%S")
-                summarization = []
-                for unit in units:
-                    count = alerts.filter(unitid=unit.id).count()
-                    if count > 0:
-                        summarization.append({
-                            'unit_name':unit.name,
-                            'unit_description':unit.description,
-                            'count':count,
-                        })
-                return render(request,'alerts/alert-history.html',{
-                    'initial_datetime':data['initial_datetime'],
-                    'final_datetime':data['final_datetime'],
-                    'selected_unit':data['unit_name'],
-                    'alert_type':data['alert_type'],
-                    'units':units,
-                    'alerts':alerts,
-                    'summarization':summarization,
-                })
-            else:
-                alerts = Alert.objects.using('history_db_replica').filter(
-                    unitid=unit.id,
-                    timestamp__gte=initial_timestamp,
-                    timestamp__lte=final_timestamp,
-                    alert_type=data['alert_type']
-                ).order_by('id')
-                for alert in alerts:
-                    alert.unit_name = unit.name
-                    alert.unit_description = unit.description
-                    dt = datetime.utcfromtimestamp(alert.timestamp)
-                    dt = gmt_conversor.convert_utctolocaltime(dt) # convertir a zona horaria
-                    alert.datetime = dt.strftime("%d/%m/%Y %H:%M:%S")
-                summarization = []
-                for unit in units:
-                    count = alerts.filter(unitid=unit.id).count()
-                    if count > 0:
-                        summarization.append({
-                            'unit_name':unit.name,
-                            'unit_description':unit.description,
-                            'count':count,
-                        })
-                return render(request,'alerts/alert-history.html',{
-                    'initial_datetime':data['initial_datetime'],
-                    'final_datetime':data['final_datetime'],
-                    'selected_unit':unit,
-                    'alert_type':data['alert_type'],
-                    'units':units,
-                    'alerts':alerts,
-                    'summarization':summarization,
-                })
-        return render(request,'alerts/alert-history.html',{
-            'units':units,
-            'form':form,
-        })
-    #GET
     units = Device.objects.filter(account=request.user.profile.account)
     return render(request,'alerts/alert-history.html',{
         'units':units,
     })
+
+def get_alert_history(request,initial_datetime,final_datetime,unit_name,alert_type):
+    units = Device.objects.filter(account=request.user.profile.account)
+    initial_timestamp = None
+    final_timestamp = None
+    unit = None
+    if unit_name.upper() != 'ALL':
+        try:
+            unit = Device.objects.get(name=unit_name)
+        except Exception as e:
+            error = {
+            'error':str(e)
+            }
+            return Response(error,status=status.HTTP_400_BAD_REQUEST)
+    #
+    try:
+        initial_datetime_str = f"{initial_datetime}:00"
+        initial_datetime_obj = datetime.strptime(initial_datetime_str, '%Y-%m-%d %H:%M:%S')
+        # convertir a zona horaria
+        initial_datetime_obj = gmt_conversor.convert_localtimetoutc(initial_datetime_obj)
+        # --
+        initial_timestamp = datetime.timestamp(initial_datetime_obj)
+    except Exception as e:
+        error = {
+            'error':str(e)
+        }
+        return Response(error,status=status.HTTP_400_BAD_REQUEST)
+    #
+    try:
+        final_datetime_str = f"{final_datetime}:00"
+        final_datetime_obj = datetime.strptime(final_datetime_str, '%Y-%m-%d %H:%M:%S')
+        # convertir a zona horaria
+        final_datetime_obj = gmt_conversor.convert_localtimetoutc(final_datetime_obj)
+        # --
+        final_timestamp = datetime.timestamp(final_datetime_obj)
+    except Exception as e:
+        error = {
+            'error':str(e)
+        }
+        return Response(error,status=status.HTTP_400_BAD_REQUEST)
+
+    if unit_name.upper() == 'ALL':
+        alerts = Alert.objects.using('history_db_replica').filter(
+            accountid=request.user.profile.account.id,
+            timestamp__gte=initial_timestamp,
+            timestamp__lte=final_timestamp,
+            alert_type=alert_type
+        ).order_by('id')
+        for alert in alerts:
+            unit = units.get(id=alert.unitid)
+            alert.unit_name = unit.name
+            alert.unit_description = unit.description
+            dt = datetime.utcfromtimestamp(alert.timestamp)
+            dt = gmt_conversor.convert_utctolocaltime(dt) # convertir a zona horaria
+            alert.datetime = dt.strftime("%d/%m/%Y %H:%M:%S")
+        summarization = []
+        for unit in units:
+            count = alerts.filter(unitid=unit.id).count()
+            if count > 0:
+                summarization.append({
+                    'unit_name':unit.name,
+                    'unit_description':unit.description,
+                    'count':count,
+                })
+        response = {
+            'alerts':alerts,
+            'summarization':summarization,
+        }
+        return Response(response,status=status.HTTP_200_OK)
+    else:
+        alerts = Alert.objects.using('history_db_replica').filter(
+            unitid=unit.id,
+            timestamp__gte=initial_timestamp,
+            timestamp__lte=final_timestamp,
+            alert_type=alert_type
+        ).order_by('id')
+        for alert in alerts:
+            alert.unit_name = unit.name
+            alert.unit_description = unit.description
+            dt = datetime.utcfromtimestamp(alert.timestamp)
+            dt = gmt_conversor.convert_utctolocaltime(dt) # convertir a zona horaria
+            alert.datetime = dt.strftime("%d/%m/%Y %H:%M:%S")
+        summarization = []
+        for unit in units:
+            count = alerts.filter(unitid=unit.id).count()
+            if count > 0:
+                summarization.append({
+                    'unit_name':unit.name,
+                    'unit_description':unit.description,
+                    'count':count,
+                })
+        response = {
+            'alerts':alerts,
+            'summarization':summarization,
+        }
+        return Response(response,status=status.HTTP_200_OK)
 
 @api_view(['GET'])
 def get_alert(request,id):
