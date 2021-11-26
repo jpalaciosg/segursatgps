c48de3a7c4f241dbb391f8304fe7ee8d39d78327
James Palacios <james.hollywood69@hotmail.com>
2021-11-26 18:16:19 -0500

    modificando consumers

--- segursatgps/routing.py
+++ segursatgps/routing.py
@@ -1,11 +1,8 @@
 from django.urls import re_path
 
-import locations.consumers as locations_consumers
-import alerts.consumers as alerts_consumers
-import generic.consumers as generic_consumers
+#import locations.consumers as locations_consumers
+from segursatgps.consumers import GenericConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/locations/(?P<room_name>\w+)/$', locations_consumers.LocationConsumer.as_asgi()),
-    re_path(r'ws/alert-listener/(?P<room_name>\w+)/$', alerts_consumers.AlertListenerConsumer.as_asgi()),
-    re_path(r'ws/reload-listener/(?P<room_name>\w+)/$', generic_consumers.ReloadListenerConsumer.as_asgi()),
+    re_path(r'ws/locations/(?P<room_name>\w+)/$', GenericConsumer.as_asgi()),
 ]
