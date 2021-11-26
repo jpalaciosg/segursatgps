32f36ce42c01980ae973ca8a0e582db708138f87
James Palacios <james.hollywood69@hotmail.com>
2021-11-26 16:34:23 -0500

    añadiendo alert y reload consumer

--- segursatgps/routing.py
+++ segursatgps/routing.py
@@ -1,7 +1,11 @@
 from django.urls import re_path
 
 import locations.consumers as locations_consumers
+import alerts.consumers as alerts_consumers
+import generic.consumers as generic_consumers
 
 websocket_urlpatterns = [
     re_path(r'ws/locations/(?P<room_name>\w+)/$', locations_consumers.LocationConsumer.as_asgi()),
+    re_path(r'ws/alert-listener/(?P<room_name>\w+)/$', alerts_consumers.AlertListenerConsumer.as_asgi()),
+    re_path(r'ws/reload-listener/(?P<room_name>\w+)/$', generic_consumers.ReloadListenerConsumer.as_asgi()),
 ]
