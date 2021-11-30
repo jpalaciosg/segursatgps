2a6f755cc7b1be098d86ad88cfb06f8758f3a208
James Palacios <james.hollywood69@hotmail.com>
2021-11-30 15:20:22 -0500

    colocando profile a los contextos de main_view

--- generic/views.py
+++ generic/views.py
@@ -4,4 +4,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def main_view(request):
-    return render(request,'generic/main.html',{})
+    profile = request.user.profile
+    return render(request,'generic/main.html',{
+        profile: profile,
+    })
