615172a8a1db6f406ade9b9034d842bb438d6cc1
James Palacios <james.hollywood69@hotmail.com>
2022-01-07 17:29:42 -0500

    cambios en los usuarios

--- users/models.py
+++ users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    email = models.EmailField('email address', unique=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    email = models.EmailField('email address',blank=True)
+    #USERNAME_FIELD = 'email'
+    #REQUIRED_FIELDS = ['username']
 
 # Create your models here.
 class Account(models.Model):
