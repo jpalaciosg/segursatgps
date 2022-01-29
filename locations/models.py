5762a67992bf70392e15e6f5e54caeb959b65485
James Palacios <james.hollywood69@hotmail.com>
2022-01-29 12:31:47 -0500

    añadiendo osinergmin

--- locations/models.py
+++ locations/models.py
@@ -67,6 +67,9 @@ class SutranLocation(models.Model):
     device_datetime = models.DateTimeField()
     server_datetime = models.DateTimeField()
     status = BigIntegerField(default=0)
+    class Meta:
+        # app_label helps django to recognize your db
+        app_label = 'locations'
 
 class OsinergminLocation(models.Model):
     id = models.BigAutoField(primary_key=True)
