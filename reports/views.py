2adadeec7a250421ecc6769762b28359fb823a25
James Palacios <james.hollywood69@hotmail.com>
2022-02-21 11:05:32 -0500

    get_temperature_report 1

--- reports/views.py
+++ reports/views.py
@@ -3063,8 +3063,8 @@ def get_temperature_report(request,unit_name,initial_datetime,final_datetime):
             'attributes':json.loads(data[i]['attributes'])
         })
         try:
-            #data[i]['temp'] = json.loads(data[i]['attributes'])['temp1']
-            data[i]['temp'] = 100
+            data[i]['temp'] = json.loads(data[i]['attributes'])['temp1']
+            temperature_report.append(data[i])
         except Exception as e:
             pass
     summarization = []
