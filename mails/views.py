ae094f421ed121392681d66b35db49036578f5b7
James Palacios <james.hollywood69@hotmail.com>
2022-03-12 12:37:50 -0500

    mail list view 2

--- mails/views.py
+++ mails/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-
-from .models import FleetTrigger
 
 from common.gmt_conversor import GMTConversor
 from common.privilege import Privilege
