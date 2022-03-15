1f88cf0cd5308698376ba39646e710c20f52a4a7
James Palacios <james.hollywood69@hotmail.com>
2022-03-15 13:49:26 -0500

    añadiendo extension 1004 y 1005 1

--- triggers/admin.py
+++ triggers/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import FleetTrigger,FleetTriggerExtension1003,FleetTriggerExtension1006,FleetTriggerExtension1007,FleetTriggerExtension1008
+from .models import FleetTrigger,FleetTriggerExtension1003,FleetTriggerExtension1004,FleetTriggerExtension1005,FleetTriggerExtension1006,FleetTriggerExtension1007,FleetTriggerExtension1008
 
 # Register your models here.
 admin.site.register([
   FleetTrigger,
   FleetTriggerExtension1003,
+  FleetTriggerExtension1004,
+  FleetTriggerExtension1005,
   FleetTriggerExtension1006,
   FleetTriggerExtension1007,
   FleetTriggerExtension1008,
