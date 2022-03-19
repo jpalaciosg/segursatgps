fd1d3fe5960b693f4131006ab9482155d3d0843b
James Palacios <james.hollywood69@hotmail.com>
2022-03-19 16:43:55 -0500

    arreglando el FleetTriggerSerializer

--- triggers/serializers.py
+++ triggers/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from mails.models import MailList
 from .models import FleetTrigger
 from users.models import Account
 
@@ -6,8 +8,13 @@ class FleetTriggerSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         account = Account.objects.get(id=validated_data['account'])
         validated_data['account'] = account
-        mail_list = FleetTrigger.objects.create(**validated_data)
-        return mail_list
+        try:
+            mail_list = MailList.objects.get(id=validated_data['mail_list'])
+            validated_data['mail_list'] = mail_list
+        except:
+            pass
+        fleet_trigger = FleetTrigger.objects.create(**validated_data)
+        return fleet_trigger
     class Meta:
         model = FleetTrigger
         fields = ('__all__')
