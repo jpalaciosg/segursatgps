f7177e8989500c0a4241778514575ae5384aab8b
James Palacios <james.hollywood69@hotmail.com>
2022-03-21 13:27:01 -0500

    mostrando el alert mail queue en el admin

--- mails/admin.py
+++ mails/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import MailList
+from .models import MailList,AlertMailQueue
 
 # Register your models here.
 admin.site.register([
   MailList,
+  AlertMailQueue,
 ])
