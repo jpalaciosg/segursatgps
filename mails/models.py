ff5eed036f6b78ffa6bfbd338161a12eb3c3c398
James Palacios <james.hollywood69@hotmail.com>
2022-03-21 11:40:40 -0500

    creando una cola de correos para las alertas

--- mails/models.py
+++ mails/models.py
@@ -1,3 +1,4 @@
+from statistics import mode
 from django.db import models
 from users.models import Account
 
@@ -14,3 +15,25 @@ class MailList(models.Model):
     def __str__(self):
         return f"{self.account}_{self.name}"
 
+class AlertMailQueue(models.Model):
+    alert_name = models.CharField(max_length=50)
+    alert_description = models.TextField(blank=True)
+    alert_timestamp = models.IntegerField()
+    alert_latitude = models.FloatField
+    alert_longitude = models.FloatField
+    alert_speed = models.IntegerField()
+    alert_angle = models.IntegerField()
+    alert_address = models.TextField(blank=True)
+    unit_name = models.CharField(max_length=50)
+    unit_description = models.CharField(max_length=50)
+    account_id = models.IntegerField()
+    customer_description = models.TextField(blank=True)
+    mails = models.TextField(blank=True)
+    it_was_sent = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    class Meta:
+        # indexes
+        indexes = [
+            models.Index(fields=['account_id', 'alert_timestamp',]),
+        ]
