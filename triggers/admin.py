756554e6a02961cdc60e927fc8adbc0727e1482e
James Palacios <james.hollywood69@hotmail.com>
2022-03-26 08:34:19 -0500

    añadiendo el trigger por unidad

--- triggers/admin.py
+++ triggers/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import FleetTrigger,Extension1003,Extension1004,Extension1005,Extension1006,Extension1007,Extension1008
+from .models import FleetTrigger,UnitTrigger,Extension1003,Extension1004,Extension1005,Extension1006,Extension1007,Extension1008
 
 # Register your models here.
 admin.site.register([
   FleetTrigger,
+  UnitTrigger,
   Extension1003,
   Extension1004,
   Extension1005,
--- triggers/models.py
+++ triggers/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from users.models import Account
 from geofences.models import Geofence
 from mails.models import MailList
+from units.models import Device
 
 ALERT_TYPE_CHOICES = [
     (1001, 'ALERTA DE PANICO'),
@@ -53,6 +54,35 @@ class Extension1008(models.Model):
     account = models.ForeignKey(Account,on_delete=models.CASCADE)
 
 class FleetTrigger(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True)
+    alert_type = models.IntegerField(
+        choices=ALERT_TYPE_CHOICES,
+    )
+    alert_priority = models.CharField(
+        max_length=9,
+        choices=PRIORITY_CHOICES,
+        default='L',
+    )
+    is_active = models.BooleanField(default=True)
+    send_notification = models.BooleanField(default=True)
+    send_mail_notification = models.BooleanField(default=True)
+    mail_list = models.ForeignKey(MailList,null=True,blank=True,on_delete=models.SET_NULL)
+    units = models.ManyToManyField(Device)
+    extension1003 = models.OneToOneField(Extension1003,null=True,blank=True,on_delete=models.CASCADE)
+    extension1004 = models.OneToOneField(Extension1004,null=True,blank=True,on_delete=models.CASCADE)
+    extension1005 = models.OneToOneField(Extension1005,null=True,blank=True,on_delete=models.CASCADE)
+    extension1006 = models.OneToOneField(Extension1006,null=True,blank=True,on_delete=models.CASCADE)
+    extension1007 = models.OneToOneField(Extension1007,null=True,blank=True,on_delete=models.CASCADE)
+    extension1008 = models.OneToOneField(Extension1008,null=True,blank=True,on_delete=models.CASCADE)
+    account = models.ForeignKey(Account,on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.account}_{self.name}"
+
+class UnitTrigger(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
