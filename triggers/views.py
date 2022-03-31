534c7a73302428fa191f6baf4304eb4dc7b318e9
James Palacios <james.hollywood69@hotmail.com>
2022-03-31 12:32:28 -0500

    añadir update_1006_unit_trigger

--- triggers/views.py
+++ triggers/views.py
@@ -619,7 +619,7 @@ def update_1006_fleet_trigger(request,id):
                     id = item,
                     account = request.user.profile.account,
                 )
-                fleet_trigger.extension1005.geofences.add(geofence)
+                fleet_trigger.extension1006.geofences.add(geofence)
             except Exception as e:
                 print(e)
         if 'mail_list' in data:
@@ -1302,6 +1302,57 @@ def update_1005_unit_trigger(request,id):
                     account = request.user.profile.account,
                 )
                 unit_trigger.extension1005.geofences.add(geofence)
+            except Exception as e:
+                print(e)
+        if 'mail_list' in data:
+            try:
+                mail_list = MailList.objects.get(
+                    id = data['mail_list'],
+                    account = request.user.profile.account,
+                )
+                unit_trigger.mail_list = mail_list
+            except Exception as e:
+                print(e)
+        for id in data['units']:
+            try:
+                device = Device.objects.get(id=id,account=request.user.profile.account)
+                unit_trigger.units.add(device)
+            except:
+                pass
+        unit_trigger.save()
+        return Response(data,status=status.HTTP_200_OK)
+    else:
+        error = {'errors':serializer.errors}
+        return Response(error,status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def update_1006_unit_trigger(request,id):
+    data = request.data
+    try:
+        unit_trigger = UnitTrigger.objects.get(id=id)
+    except Exception as e:
+        error = {
+            'detail': str(e)
+        }
+        return Response(error,status=status.HTTP_400_BAD_REQUEST)
+    serializer = UnitTrigger1006Serializer(data=data)
+    if serializer.is_valid():
+        unit_trigger.name = data['name']
+        unit_trigger.description = data['description']
+        unit_trigger.alert_type = 1006
+        unit_trigger.alert_priority = data['alert_priority']
+        unit_trigger.is_active = data['is_active']
+        unit_trigger.send_notification = data['send_notification']
+        unit_trigger.send_mail_notification = data['send_mail_notification']
+        unit_trigger.extension1006.speed = data['speed']
+        unit_trigger.extension1006.geofences.clear()
+        for item in data['geofences']:
+            try:
+                geofence = Geofence.objects.get(
+                    id = item,
+                    account = request.user.profile.account,
+                )
+                unit_trigger.extension1006.geofences.add(geofence)
             except Exception as e:
                 print(e)
         if 'mail_list' in data:
