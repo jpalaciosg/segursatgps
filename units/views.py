e55c70e49a2a7d53746a6d0ae9e34072987925bb
James Palacios <james.hollywood69@hotmail.com>
2022-03-31 20:47:58 -0500

    algunos cambios en las unidades

--- units/views.py
+++ units/views.py
@@ -28,139 +28,6 @@ def units_view(request):
     if privilege.view_latest_alerts(request.user.profile) == False:
         return HttpResponse("<h1>Acceso restringido</h1>", status=403)
     # fin - verificar privilegios
-    if request.method == 'POST':
-        data = request.POST
-        data._mutable = True
-        # Unit create
-        if data['form_type'] == 'create_form':
-            units = privilege.get_units(request.user.profile)
-            create_form = UnitCreateForm(data)
-            if create_form.is_valid():
-                data = create_form.cleaned_data
-                try:
-                    unit = Device.objects.get(
-                        name = data['unit_name'],
-                        account = request.user.profile.account
-                    )
-                except Device.DoesNotExist:
-                    unit = None
-                if unit:
-                    create_form.add_error('unit_name', 'La unidad ya existe.')
-                if not unit:
-                    try:
-                        unit = Device.objects.create(
-                            name = data['unit_name'],
-                            description = data['description'],
-                            uniqueid = data['uniqueid'],
-                            imei = data['imei'],
-                            sim_phonenumber = data['sim_phonenumber'],
-                            sim_iccid = data['sim_iccid'],
-                            odometer = data['odometer'],
-                            note = data['note'],
-                            account = request.user.profile.account
-                        )
-                        create_form = UnitCreateForm()
-                        return render(request,'units/units.html',{
-                            'units':units,
-                            'create_form':create_form,
-                            'success':'Unidad creada exitosamente.'
-                        })
-                    except IntegrityError as e:
-                        print(e)
-                        field = e.args[1].split('.')[1].replace("'","")
-                        if field == 'imei': 
-                            create_form.add_error('imei', 'imei ya existe.')
-                        if field == 'uniqueid': 
-                            create_form.add_error('uniqueid', 'identificador unico ya existe.')
-            return render(request,'units/units.html',{
-                'units':units,
-                'create_form':create_form
-            })
-        # Unit update
-        if data['form_type'] == 'update_form':
-            units = privilege.get_units(request.user.profile)
-            create_form = UnitCreateForm()
-            update_form = UnitUpdateForm(data,auto_id=False)
-            try:
-                data['odometer'] = float(data['odometer'])
-            except Exception as e:
-                print(e)
-            if update_form.is_valid():
-                form_data = update_form.cleaned_data
-                try:
-                    unit = Device.objects.get(
-                        id=form_data['id'],
-                        account=request.user.profile.account,
-                    )
-                except Device.DoesNotExist:
-                    unit = None
-                if unit:
-                    try:
-                        unit.name = form_data['unit_name']
-                        unit.uniqueid = form_data['uniqueid']
-                        unit.imei = form_data['imei']
-                        unit.sim_phonenumber = form_data['sim_phonenumber']
-                        unit.sim_iccid = form_data['sim_iccid']
-                        unit.description = form_data['description']
-                        unit.odometer = form_data['odometer']
-                        unit.note = form_data['note']
-                        unit.save()
-                        for unit in units:
-                            unit.odometer = str(unit.odometer)
-                            unit.modified = gmt_conversor.convert_utctolocaltime(unit.modified) # convertir a zona horaria
-                            unit.created = gmt_conversor.convert_utctolocaltime(unit.created) # convertir a zona horaria
-                        return render(request,'units/units.html',{
-                            'units':units,
-                            'create_form':create_form,
-                            'update_form':update_form,
-                            'success':'Unidad actualizada exitosamente.'
-                        })
-                    except IntegrityError as e:
-                        print(e)
-                        field = e.args[1].split('.')[1].replace("'","")
-                        if field == 'imei': 
-                            update_form.add_error('imei', 'imei ya existe.')
-                        if field == 'uniqueid': 
-                            update_form.add_error('uniqueid', 'identificador unico ya existe.')
-                    for unit in units:
-                        unit.odometer = str(unit.odometer)
-                        unit.modified = gmt_conversor.convert_utctolocaltime(unit.modified) # convertir a zona horaria
-                        unit.created = gmt_conversor.convert_utctolocaltime(unit.created) # convertir a zona horaria
-                    return render(request,'units/units.html',{
-                        'units':units,
-                        'create_form':create_form,
-                        'update_form':update_form,
-                        'modal_id':f'unit{unit.id}_update_modal',
-                        'modal_unit_id':unit.id
-                    })
-                else:
-                    return HttpResponse(status=500)
-            else:
-                try:
-                    unit = Device.objects.get(
-                        id=data['id'],
-                        account=request.user.profile.account,
-                    )
-                except Device.DoesNotExist:
-                    unit = None
-                if unit:
-                    #update_form.fields['id'].widget = forms.HiddenInput()
-                    for unit in units:
-                        unit.odometer = str(unit.odometer)
-                        unit.modified = gmt_conversor.convert_utctolocaltime(unit.modified) # convertir a zona horaria
-                        unit.created = gmt_conversor.convert_utctolocaltime(unit.created) # convertir a zona horaria
-                    return render(request,'units/units.html',{
-                        'units':units,
-                        'create_form':create_form,
-                        'update_form':update_form,
-                        'modal_id':f'unit{unit.id}_update_modal',
-                        'modal_unit_id':unit.id
-                    })
-                else:
-                    return HttpResponse(status=500)
-                     
-    # GET
-    create_form = UnitCreateForm()
     units = privilege.get_units(request.user.profile)
     """
     for unit in units:
@@ -172,11 +39,10 @@ def units_view(request):
     """
     for unit in units:
         unit.odometer = str(unit.odometer)
-        unit.modified = gmt_conversor.convert_utctolocaltime(unit.modified).strftime("%d/%m/%Y %H:%M:%S") # convertir a zona horaria
-        unit.created = gmt_conversor.convert_utctolocaltime(unit.created).strftime("%d/%m/%Y %H:%M:%S") # convertir a zona horaria
+        unit.modified = gmt_conversor.convert_utctolocaltime(unit.modified).strftime("%d/%m/%Y %H:%M:%S")
+        unit.created = gmt_conversor.convert_utctolocaltime(unit.created).strftime("%d/%m/%Y %H:%M:%S")
     return render(request,'units/units.html',{
         'units':units,
-        'create_form':create_form
     })
 
 @login_required
@@ -196,7 +62,11 @@ def get_units(request):
     try:
         units = privilege.get_units(request.user.profile)
         serializer = DeviceSerializer(units,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
+        data = serializer.data
+        for i in range(len(data)):
+            data[i]['modified'] = gmt_conversor.convert_utctolocaltime(units[i].modified).strftime("%d/%m/%Y %H:%M:%S")
+            data[i]['created'] = gmt_conversor.convert_utctolocaltime(units[i].created).strftime("%d/%m/%Y %H:%M:%S")
+        return Response(data,status=status.HTTP_200_OK)
     except Exception as e:
         error = {'error':str(e)}
         return Response(error,status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -222,6 +92,8 @@ def get_unit(request,name):
         last_report = datetime.fromtimestamp(unit.last_timestamp)
         last_report = gmt_conversor.convert_utctolocaltime(last_report)
         data['last_report'] = last_report.strftime("%d-%m-%Y %H:%M:%S")
+        data['modified'] = gmt_conversor.convert_utctolocaltime(unit.modified).strftime("%d/%m/%Y %H:%M:%S")
+        data['created'] = gmt_conversor.convert_utctolocaltime(unit.created).strftime("%d/%m/%Y %H:%M:%S")
         return Response(data,status=status.HTTP_200_OK)
     except Exception as e:
         print(e)
@@ -259,14 +131,6 @@ def update_unit(request,id):
         error = {'errors':serializer.errors}
         return Response(error,status=status.HTTP_400_BAD_REQUEST)
 
-@login_required
-def delete_unit(request,id):
-    try:
-        unit = Device.objects.get(id=id,account=request.user.profile.account)
-        unit.delete()
-        return redirect('units')
-    except:
-        return redirect('units')
 
 @api_view(['POST'])
 def create_group(request):
