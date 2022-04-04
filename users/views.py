166b5663cac6e0d283daae06f56ade639cc04099
James Palacios <james.hollywood69@hotmail.com>
2022-04-04 11:51:35 -0500

    añadiendo el update-user 2

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 
 from rest_framework.response import Response
 from rest_framework import status
