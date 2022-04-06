fa4f0a58aa6e1e207ebcb094d573ce2eec182d4e
James Palacios <james.hollywood69@hotmail.com>
2022-04-06 18:21:02 -0500

    probando el reporte de horas 9

--- reports/views.py
+++ reports/views.py
@@ -3548,7 +3548,7 @@ def get_hours_report(request):
                 'address': location.address,
             }
             try:
-                c_time = int(json.loads(location.attributes)['io449'])
+                c_time = device_reader.get_hours({'attributes':json.loads(location.attributes)})
                 hours = int(c_time/3600)
                 minutes = int(c_time%3600/60)
                 item['hours'] = f"{hours} h {minutes} m"
