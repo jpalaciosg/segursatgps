be9bd9af0d14f7fc66b7fd47dad3d4785a581fe5
James Palacios <james.hollywood69@hotmail.com>
2022-05-02 07:11:24 -0500

    modificaciones de urgewncia sutran

--- locations/tasks.py
+++ locations/tasks.py
@@ -58,19 +58,21 @@ def insert_location_in_history(data):
     # INTRODUCIR UBICACION SUTRAN
     if data['account'] == 'civa' or data['account'] == 'alcas' or data['account'] == 'miskymayo':
         try:
-            if int(data['speed'] > 0): event = 'EN'
-            else: event = 'PA'
+            event = 'EN' if int(data['speed'] > 0) else 'PA'
             timestamp = data['timestamp']
             device_datetime = datetime.utcfromtimestamp(timestamp)
             device_datetime = gmt_conversor.convert_utctolocaltime(device_datetime)
+            speed = 95 if int(data['speed']) > 95 else data['speed']
             SutranLocation.objects.create(
                 unit_name = data['unit_name'],
                 latitude = data['latitude'],
                 longitude = data['longitude'],
                 angle = data['angle'],
-                speed = data['speed'],
+                #speed = data['speed'],
+                speed = speed,
                 event = event,
-                device_datetime = device_datetime,
+                #device_datetime = device_datetime,
+                device_datetime = gmt_conversor.convert_utctolocaltime(datetime.utcnow())
                 server_datetime = gmt_conversor.convert_utctolocaltime(datetime.utcnow()),
             )
         except:
