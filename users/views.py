4cd401c8876c3bc26a5564d9519e951927940a31
James Palacios <james.hollywood69@hotmail.com>
2022-05-09 16:22:41 -0500

    corrgiendo el update de los usuarios

--- users/views.py
+++ users/views.py
@@ -291,6 +291,7 @@ def update_profile(request,id):
         profile.view_triggers = data['view_triggers']
         profile.view_geofences = data['view_geofences']
         profile.view_users = data['view_users']
+        profile.units.clear()
         for id in data['units']:
             try:
                 device = Device.objects.get(id=id,account=request.user.profile.account)
