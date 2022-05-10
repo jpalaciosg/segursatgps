b91a41100f2104895e9fb811a718047e89c84e8b
James Palacios <james.hollywood69@hotmail.com>
2022-05-10 18:44:36 -0500

    modificaciones en osinergmin 1

--- scripts/send_osinergmin_data1.py
+++ scripts/send_osinergmin_data1.py
@@ -35,27 +35,26 @@ print(len(locations))
 
 for location in locations:
     try:
-        if location.speed > 0:
-            dt = datetime.utcfromtimestamp(location.timestamp)
-            #dt = datetime.utcnow()
-            dt_str = dt.strftime("%Y-%m-%dT%H:%M:%S.000Z")
-            item = {
-                "event": "none",
-                "plate": location.reference,
-                "speed": location.speed,
-                "odometer": 0,
-                "position": {
-                    "latitude": location.latitude,
-                    "longitude": location.longitude,
-                    "altitude": location.altitude
-                },
-                "gpsDate": dt_str,
-                "tokenTrama": TOKEN
-            }
-            item = json.dumps(item)
-            print(item)
-            x = threading.Thread(target=thread_function, args=(item,location.id,))
-            x.start()
+        dt = datetime.utcfromtimestamp(location.timestamp)
+        #dt = datetime.utcnow()
+        dt_str = dt.strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        item = {
+            "event": "none",
+            "plate": location.reference,
+            "speed": location.speed,
+            "odometer": 0,
+            "position": {
+                "latitude": location.latitude,
+                "longitude": location.longitude,
+                "altitude": location.altitude
+            },
+            "gpsDate": dt_str,
+            "tokenTrama": TOKEN
+        }
+        item = json.dumps(item)
+        print(item)
+        x = threading.Thread(target=thread_function, args=(item,location.id,))
+        x.start()
     except Exception as e:
         print('ERROR:')
         print(e)
