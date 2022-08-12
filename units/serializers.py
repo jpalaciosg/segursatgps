3b22dc9ee5c3c2203d55234a7943e12251a28ba6
James Palacios <james.hollywood69@hotmail.com>
2022-08-12 16:30:22 -0500

    agregando create-group 4

--- units/serializers.py
+++ units/serializers.py
@@ -16,6 +16,8 @@ class GroupSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         account = Account.objects.get(id=validated_data['account'])
         validated_data['account'] = account
+        units = validated_data['units']
+        del validated_data['units']
         return Group.objects.create(**validated_data)
     class Meta:
         model = Group
