37cd721d3277ec8da7f06e8a8ea49748eec37227
James Hollywood <james.hollywood69@hotmail.com>
2022-10-17 21:21:14 +0000

    modificando privileges

--- common/privilege.py
+++ common/privilege.py
@@ -1,178 +1,191 @@
 from units.models import Device
+from users.models import Account
 
 class Privilege:
 
-    def get_units(self,profile):
-        is_admin = profile.is_admin
+    def get_units(self,request):
+        is_admin = request.user.profile.is_admin
         units = None
         if is_admin:
-            units = Device.objects.filter(account=profile.account)
+            if request.user.profile.account.name == '20100176450':
+                units = []
+                solgas_units = Device.objects.filter(account=request.user.profile.account)
+                for unit in solgas_units:
+                    unit.description = f'SOLGAS - {unit.description}'
+                    units.append(unit)
+                account = Account.objects.get(name='10296954077')
+                yucra_units = Device.objects.filter(account=account)
+                for unit in yucra_units:
+                    unit.description = f'YUCRA - {unit.description}'
+                    units.append(unit)
+            else:
+                units = Device.objects.filter(account=request.user.profile.account)
         else:
-            units = profile.units.all()
+            units = request.user.profile.units.all()
         return units
 
-    def view_detailed_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_detailed_report == False:
+    def view_detailed_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_detailed_report == False:
                 return False
         return True
 
-    def view_speed_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_speed_report == False:
+    def view_speed_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_speed_report == False:
                 return False
         return True
 
-    def view_stop_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_stop_report == False:
+    def view_stop_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_stop_report == False:
                 return False
         return True
 
-    def view_trip_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_trip_report == False:
+    def view_trip_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_trip_report == False:
                 return False
         return True
 
-    def view_day_trip_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_day_trip_report == False:
+    def view_day_trip_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_day_trip_report == False:
                 return False
         return True
 
-    def view_mileage_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_mileage_report == False:
+    def view_mileage_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_mileage_report == False:
                 return False
         return True
 
-    def view_geofence_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_geofence_report == False:
+    def view_geofence_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_geofence_report == False:
                 return False
         return True
 
-    def view_driving_style_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_driving_style_report == False:
+    def view_driving_style_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_driving_style_report == False:
                 return False
         return True
 
-    def view_telemetry_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_telemetry_report == False:
+    def view_telemetry_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_telemetry_report == False:
                 return False
         return True
 
-    def view_detailed_report_with_attributes(self,profile):
-        if profile.is_admin == False:
-            if profile.view_detailed_report_with_attributes == False:
+    def view_detailed_report_with_attributes(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_detailed_report_with_attributes == False:
                 return False
         return True
 
-    def view_group_trip_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_group_trip_report == False:
+    def view_group_trip_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_group_trip_report == False:
                 return False
         return True
 
-    def view_group_stop_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_group_stop_report == False:
+    def view_group_stop_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_group_stop_report == False:
                 return False
         return True
 
-    def view_group_mileage_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_group_mileage_report == False:
+    def view_group_mileage_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_group_mileage_report == False:
                 return False
         return True
 
-    def view_group_speed_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_group_speed_report == False:
+    def view_group_speed_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_group_speed_report == False:
                 return False
         return True
 
-    def view_group_geofence_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_group_geofence_report == False:
+    def view_group_geofence_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_group_geofence_report == False:
                 return False
         return True
 
-    def view_units(self,profile):
-        if profile.is_admin == False:
-            if profile.view_units == False:
-                return False
-        return True
-
-    #Eslim
-    def view_units_group(self,profile):
-        if profile.is_admin == False:
-            if profile.view_units_group == False:
-                return False
-        return True
-    #Eslim
-
-    def view_unit_triggers(self,profile):
-        if profile.is_admin == False:
-            if profile.view_unit_triggers == False:
-                return False
-        return True
-
-    def view_fleet_triggers(self,profile):
-        if profile.is_admin == False:
-            if profile.view_fleet_triggers == False:
-                return False
-        return True
-
-    def view_mail_lists(self,profile):
-        if profile.is_admin == False:
-            if profile.view_mail_lists == False:
-                return False
-        return True
-
-    def view_geofences(self,profile):
-        if profile.is_admin == False:
-            if profile.view_geofences == False:
+    def view_units(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_units == False:
                 return False
         return True
 
     #Eslim
-    def view_geofences_group(self,profile):
-        if profile.is_admin == False:
-            if profile.view_geofences_group == False:
+    def view_units_group(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_units_group == False:
                 return False
         return True
     #Eslim
 
-    def view_users(self,profile):
-        if profile.is_admin == False:
-            if profile.view_users == False:
+    def view_unit_triggers(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_unit_triggers == False:
                 return False
         return True
 
-    def view_latest_alerts(self,profile):
-        if profile.is_admin == False:
-            if profile.view_latest_alerts == False:
+    def view_fleet_triggers(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_fleet_triggers == False:
                 return False
         return True
 
-    def view_alert_history(self,profile):
-        if profile.is_admin == False:
-            if profile.view_alert_history == False:
+    def view_mail_lists(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_mail_lists == False:
                 return False
         return True
 
-    def view_hours_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_hours_report == False:
+    def view_geofences(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_geofences == False:
                 return False
         return True
 
-    def view_temperature_report(self,profile):
-        if profile.is_admin == False:
-            if profile.view_temperature_report == False:
+    #Eslim
+    def view_geofences_group(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_geofences_group == False:
+                return False
+        return True
+    #Eslim
+
+    def view_users(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_users == False:
+                return False
+        return True
+
+    def view_latest_alerts(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_latest_alerts == False:
+                return False
+        return True
+
+    def view_alert_history(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_alert_history == False:
+                return False
+        return True
+
+    def view_hours_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_hours_report == False:
+                return False
+        return True
+
+    def view_temperature_report(self,request):
+        if request.user.profile.is_admin == False:
+            if request.user.profile.view_temperature_report == False:
                 return False
         return True
