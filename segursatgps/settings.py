52dc2496bb3dfbddb285be13001088c447ca7371
James Palacios <james.hollywood69@hotmail.com>
2022-10-18 19:10:05 -0500

    CELERY_RESULT_EXPIRES

--- segursatgps/settings.py
+++ segursatgps/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8rubv9!m-^=(!a-zn^fato38(bkq930n@y$$31xr_5k-hbp8&c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -204,7 +204,7 @@ CHANNEL_LAYERS = {
 # celery settings
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_RESULT_EXPIRES = 600
+CELERY_RESULT_EXPIRES = 1800
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
