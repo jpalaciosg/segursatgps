750da449e7ebf761e426a76fdec6500ca4bd1dff
James Hollywood <james.hollywood69@hotmail.com>
2023-01-03 19:53:01 +0000

    añadiendo positiondb_replica

--- segursatgps/settings.py
+++ segursatgps/settings.py
@@ -144,6 +144,13 @@ DATABASES = {
             'read_default_file': f'{BASE_DIR}/segursatgps/my4.cnf',
         },
     },
+    'positiondb_replica': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'read_default_file': f'{BASE_DIR}/segursatgps/my5.cnf',
+        },
+    },
 }
 
 # Password validation
