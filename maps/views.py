0aef2ec5c0164afe5ee1a0ef2068bc539d7c2192
James Hollywood <james.hollywood69@hotmail.com>
2023-01-20 23:04:41 +0000

    añadiendo mapas multiples

--- maps/views.py
+++ maps/views.py
@@ -24,3 +24,45 @@ def mobile_map_view(request):
     return render(request,'maps/mobile.html',{
         'units':units,
     })
+
+@login_required
+def multiple_unit_map_4_view(request):
+    disable_navbar = request.GET.get('disablenavbar',None)
+    try:
+        disable_navbar = bool(int(disable_navbar))
+    except Exception as e:
+        disable_navbar = False
+    navbar = not disable_navbar
+    number_of_maps = 4
+    return render(request,'maps/multiple-map.html',{
+        'navbar':navbar,
+        'number_of_maps':number_of_maps,
+    })
+
+@login_required
+def multiple_unit_map_6_view(request):
+    disable_navbar = request.GET.get('disablenavbar',None)
+    try:
+        disable_navbar = bool(int(disable_navbar))
+    except Exception as e:
+        disable_navbar = False
+    navbar = not disable_navbar
+    number_of_maps = 6
+    return render(request,'maps/multiple-map.html',{
+        'navbar':navbar,
+        'number_of_maps':number_of_maps,
+    })
+
+@login_required
+def multiple_unit_map_8_view(request):
+    disable_navbar = request.GET.get('disablenavbar',None)
+    try:
+        disable_navbar = bool(int(disable_navbar))
+    except Exception as e:
+        disable_navbar = False
+    navbar = not disable_navbar
+    number_of_maps = 8
+    return render(request,'maps/multiple-map.html',{
+        'navbar':navbar,
+        'number_of_maps':number_of_maps,
+    })
